912e5a41e697cb82b81c311202788d81a01cfda1
wlwlsus <159632_@naver.com>
2022-03-09 17:45:36 +0900

    [add] Display List of Recipe

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,9 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -16,23 +11,16 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-	private final CategoryRepository categoryRepository;
-	private final UnitOfMeasureRepository unitOfMeasureRepository;
+	private final RecipeService recipeService;
 
-	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-		this.categoryRepository = categoryRepository;
-		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	public IndexController(RecipeService recipeService) {
+		this.recipeService = recipeService;
 	}
 
 	@RequestMapping({"", "/", "/index"})
-	public String getIndexPage() {
+	public String getIndexPage(Model model) {
 
-		Optional<List<Category>> categoryOptional = categoryRepository.findByDescriptionQuery("Fast Food");
-//		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
-		categoryOptional.ifPresent(
-						categories -> categories.forEach(category -> System.out.println("Cat id " + category.getId() + " is : " + category.getDescription()))
-		);
-//		System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+		model.addAttribute("recipes", recipeService.getRecipes());
 
 		return "index";
 	}
